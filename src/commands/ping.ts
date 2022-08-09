1baeff3380146fcc4aa4241b5a672ca6d65cb4d5
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2022-08-09 23:17:59 +0300

    Changed editReply to reply in "ping" command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,7 +4,7 @@ import { CommandData } from "../interfaces/CommandData";
 export default <CommandData> {
     data: new SlashCommandBuilder().setName("ping").setDescription("Pong!"),
     executeSlash: (interaction) => {
-        interaction.editReply("Pong!");
+        interaction.reply("Pong!");
     },
     executeDefault: (message) => {
         message.reply("Pong!");
